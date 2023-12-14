384b772f18c7de3f256dd7233b4abede5a6b1249
Vaishnavi <work.vaishnavik@gmail.com>
2023-12-13 18:01:34 -0600

    initial first chapter completed

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import pygame
 import sys
+import textwrap
+import pygame.mixer
+
 
 # Initialize Pygame
 pygame.init()
+pygame.mixer.init()
+
+pygame.mixer.music.load("assets/intro_music.mp3")
+
+
+# Get the screen size
+screen_info = pygame.display.Info()
+screen_width, screen_height = screen_info.current_w, screen_info.current_h
 
 # Set up display
-screen_width, screen_height = 800, 600
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
 pygame.display.set_caption("Raiders of the Lost Artifact")
 
 # Colors
@@ -27,27 +37,60 @@ current_chapter = 1
 # Chapter data
 chapters = {
     "intro": {
-        "text": "Welcome to Raiders of the Lost Artifact!\n\nYour goal is to find the ancient Scepter of Eternity hidden deep within the Amazon rainforest.\n\nMake choices to navigate through the challenges and uncover the mysteries.",
+        "text": "Welcome to Raiders of the Lost Artifact! \n Your goal is to find the ancient Scepter of Eternity hidden deep within the Amazon rainforest. \n Make choices to navigate through the challenges and uncover the mysteries.",
         "button_text": "Start",
-        "background": pygame.image.load("intro_background.jpg"),
+        "background": pygame.transform.scale(pygame.image.load("assets/images/intro_background.jpg"), (screen_width, screen_height)),
         "next_screen": "chapter1"
     },
     "chapter1": {
-        "text": "You find a cryptic message leading you to the hidden temple.\n\nSelect your path:",
+        "text": "You find a cryptic message leading you to the hidden temple. Select your path:",
         "choices": ["Take the winding river route.", "Repel down the cliff."],
-        "background": pygame.image.load("temple_background.jpg"),
+        "background": pygame.transform.scale(pygame.image.load("assets/images/river_background.jpg"), (screen_width, screen_height)),
         "next_chapters": [2, 3]  # Corresponding to the choices
     },
     # Add more chapters as needed
 }
 
+def wrap_text(text, font, max_width):
+    words = text.split()
+    wrapped_lines = []
+    current_line = []
+
+    for word in words:
+        # Check the width of the line with the new word
+        test_line = current_line + [word]
+        test_text = ' '.join(test_line)
+        text_width, _ = font.size(test_text)
+
+        # If the line exceeds the maximum width, start a new line
+        if text_width <= max_width:
+            current_line.append(word)
+        else:
+            wrapped_lines.append(' '.join(current_line))
+            current_line = [word]
+
+    wrapped_lines.append(' '.join(current_line))
+    return wrapped_lines
+
 def display_text(text, y_offset=0):
-    text_surface = font.render(text, True, white)
+    wrapped_text = wrap_text(text, font, screen_width - 400)  # Adjust the margin as needed
+    text_surface = pygame.Surface((screen_width - 200, screen_height - 700), pygame.SRCALPHA)
     text_rect = text_surface.get_rect(center=(screen_width // 2, screen_height // 2 + y_offset))
-    
-    # Create a background rectangle behind the text
-    pygame.draw.rect(screen, black, (text_rect.x - 10, text_rect.y - 10, text_rect.width + 20, text_rect.height + 20))
+
+    line_height = font.get_linesize()
+    total_lines = len(wrapped_text)
+    for i, line in enumerate(wrapped_text):
+        line_surface = font.render(line, True, white)
+        line_rect = line_surface.get_rect(center=(text_rect.width // 2, text_rect.height // 2 + i * line_height - (total_lines-1) * line_height // 2))
+        text_surface.blit(line_surface, line_rect)
+
+    black_rect = pygame.Surface((text_rect.width + 20, text_rect.height + 20), pygame.SRCALPHA)
+    pygame.draw.rect(black_rect, (0, 0, 0, 150), black_rect.get_rect())  # Adjust the alpha value (150) as needed
+    screen.blit(black_rect, text_rect.topleft)
+
     screen.blit(text_surface, text_rect.topleft)
+
+
 
 def display_button(text, button_rect):
     button_text = font.render(text, True, white)
@@ -55,16 +98,23 @@ def display_button(text, button_rect):
     # Draw a background rectangle and check if the mouse is over it
     highlight = button_rect.collidepoint(pygame.mouse.get_pos())
     pygame.draw.rect(screen, highlight_color if highlight else black, button_rect)
-    screen.blit(button_text, (button_rect.x + 10, button_rect.y + 10))
+    screen.blit(button_text, (button_rect.x + button_rect.width // 2 - button_text.get_width() // 2,
+                              button_rect.y + button_rect.height // 2 - button_text.get_height() // 2))
 
 def main():
     global current_screen, current_chapter
+    pygame.mixer.music.play(-1)  # The '-1' parameter means the music will loop indefinitely
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
 
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Check for left mouse button click
                 if current_screen in chapters:
@@ -95,20 +145,31 @@ def main():
 
             elif "button_text" in chapter_data:
                 # Display a button
-                chapter_data["button_rect"] = pygame.Rect(50, 400, 300, 50)
+                chapter_data["button_rect"] = pygame.Rect(screen_width // 2 - 150, screen_height - 100, 300, 50)
                 display_button(chapter_data["button_text"], chapter_data["button_rect"])
 
         pygame.display.flip()
         clock.tick(30)
 
 def display_choices(choices, choice_rects):
+    total_width = len(choices) * 300 + (len(choices) - 1) * 10
+    start_x = (screen_width - total_width) // 2  # Center the choices horizontally
+
     for i, (choice, rect) in enumerate(zip(choices, choice_rects)):
         choice_text = font.render(choice, True, white)
-        
-        # Draw a background rectangle and check if the mouse is over it
-        highlight = rect.collidepoint(pygame.mouse.get_pos())
-        pygame.draw.rect(screen, highlight_color if highlight else black, rect)
-        screen.blit(choice_text, (rect.x + 10, rect.y + 10))
+
+        # Calculate text position to center it within the button
+        text_x = start_x + i * 310 + (300 - choice_text.get_width()) // 2
+        text_y = screen_height - 70 - (50 - choice_text.get_height()) // 2
+
+        # Create a smaller surface to draw the background rectangle
+        button_surface = pygame.Surface((320, 50), pygame.SRCALPHA)
+        pygame.draw.rect(button_surface, highlight_color if rect.collidepoint(pygame.mouse.get_pos()) else black, button_surface.get_rect())
+        screen.blit(button_surface, (start_x + i * 310 + (300 - choice_text.get_width()) // 2, screen_height - 70 - (50 - choice_text.get_height()) // 2))
+
+        # Blit the choice text onto the screen
+        screen.blit(choice_text, (text_x, text_y))
+
 
 if __name__ == "__main__":
     main()
